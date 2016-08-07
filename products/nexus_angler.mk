33c6e68df1e0e671c056e474ec612d8bbc8f516c
tornado76 <oscaraigf@gmail.com>
2016-08-07 03:01:57 +0200

    Add optimizations 6P

--- products/nexus_angler.mk
+++ products/nexus_angler.mk
@@ -30,3 +30,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20F/3031278:user/release-keys \
     PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20F 3031278 release-keys"
+
+#Optimizations
+CLANG_O3 := true
+STRICT_ALIASING := true
+GRAPHITE_OPTS := true
+ENABLE_GCCONLY := true
